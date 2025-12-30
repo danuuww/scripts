88fffbe5366d26cc945f02ee289034bd1c18747b
danuuww <tdanu8@gmail.com>
2025-12-30 20:03:58 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -6,20 +6,20 @@ function BluuHub:GetConfig()
     if id == 9788848685 then
         return {
             name = "Indo Hangout",
-            kind = "l",
-            key  = "09534ae3e8db9d623bef720f8270c4f5"
+            r    = "l",
+            k    = "09534ae3e8db9d623bef720f8270c4f5"
         }
     elseif id == 93978595733734 then
         return {
             name = "Violence District",
-            kind = "l",
-            key  = "76f99941bc74264ef2d48991b5783f95"
+            r    = "l",
+            k    = "76f99941bc74264ef2d48991b5783f95"
         }
     elseif id == 86096929771195 then
         return {
             name = "Indo Beach",
-            kind = "g",
-            key  = "danuuww/scripts/refs/heads/main/9128727.lua"
+            r    = "g",
+            k    = "danuuww/scripts/refs/heads/main/9128727.lua"
         }
     end
 end
@@ -28,10 +28,10 @@ if not cfg then
     return
 end
 local url
-if cfg.kind == "l" then
-    url = "https://api.luarmor.net/files/v3/loaders/" .. cfg.key .. ".lua"
-elseif cfg.kind == "g" then
-    url = "https://raw.githubusercontent.com/" .. cfg.key
+if cfg.r == "l" then
+    url = "https://api.luarmor.net/files/v3/loaders/" .. cfg.k .. ".lua"
+elseif cfg.r == "g" then
+    url = "https://raw.githubusercontent.com/" .. cfg.k
 else
     return
 end
